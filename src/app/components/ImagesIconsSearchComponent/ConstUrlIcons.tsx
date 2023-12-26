66be25b10a3e6efdfac5963f9e2792c309db5f29
Celinda Cueto <71739893+celycueto-fx@users.noreply.github.com>
2023-12-26 14:46:07 -0500

    Update ConstUrlIcons.tsx

--- src/app/components/ImagesIconsSearchComponent/ConstUrlIcons.tsx
+++ src/app/components/ImagesIconsSearchComponent/ConstUrlIcons.tsx
@@ -1,3 +1,3 @@
-export const URL_ICON_SHARE='https://cdn-user-icons.flaticon.com/132278/132278048/1703612044208.svg?token=exp=1703613035~hmac=f6811fb8d61ffef48ef0f41694134342';
-export const URL_ICON_FAVORITE ='https://cdn-user-icons.flaticon.com/132278/132278048/1703612092407.svg?token=exp=1703613035~hmac=bc869ebc04e94ad92d9d58d68ca5ae01';
-export const URL_ICON_SAVED ='https://cdn-user-icons.flaticon.com/132278/132278048/1703612078744.svg?token=exp=1703613035~hmac=1a3e7efa76e45cb5de8b3a5609929c56';
+export const URL_ICON_SHARE='https://cdn-user-icons.flaticon.com/40608/40608996/1703611786369.svg?token=exp=1703620463~hmac=f7e6d7b6d8a7cdf999ee1b25436faa9a';
+export const URL_ICON_FAVORITE ='https://cdn-user-icons.flaticon.com/132278/132278048/1703612092407.svg?token=exp=1703620745~hmac=372add009650aad27561a0f7262c3ae4';
+export const URL_ICON_SAVED ='https://cdn-user-icons.flaticon.com/132278/132278048/1703612078744.svg?token=exp=1703620745~hmac=ac1687b1afaee61dc66201ba3e68354356';
